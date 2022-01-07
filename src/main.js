c870a8b047fa67ed47bac0bac1646b8037e3a611
Hoang Nam <namhoangde2002@gmail.com>
2022-01-07 17:10:04 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+const menulist =  ["menu 1", "menu 2", "menu 3"];
+
+//b1: selector
+//b2: loop
+//b3: render
+const menuElement = document.querySelector('#menu');
+if (menuElement){
+  for(let i = 0;  i< menulist.length; i++){
+    menuElement.innerHTML += `<li>${menulist[i]}</li>`
+  }
+
+  // for
+}
+// template string
