445fd0df96422c2cdc652c2dba3828fcc8cab235
Hoang Nam <namhoangde2002@gmail.com>
2022-01-10 17:59:14 +0700

    hehe

--- src/page/home.js
+++ src/page/home.js
@@ -8,7 +8,7 @@ const HomePage = {
       <h2 class="uppercase text-lg font-bold my-4">Tin tức học tập</h2>
       <div class="grid grid-cols-3 gap-8 ">
         <div class="border border-gray-400 p-4 ">
-          <a href=""><img src="../.img/img1.jfif" class="mx-auto" alt=""></a>
+          <a href=""><img src="../../img/img1.jfif" class="mx-auto" alt=""></a>
           <a href="">
             <h3 class="text-base text-red-600 font-bold my-2 ml-3">Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018</h3>
           </a>
@@ -16,7 +16,7 @@ const HomePage = {
             tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt</p>
         </div>
         <div class="border border-gray-400 p-4">
-          <a href=""><img src="./img/img 2.jfif" class="mx-auto " alt=""></a>
+          <a href=""><img src="../../img/img 2.jfif" class="mx-auto " alt=""></a>
           <a href="">
             <h3 class="text-base text-red-600 font-bold my-2 ml-3">Sinh viên khối ngành Kinh tế học cách “chạm” vào cảm xúc</h3>
           </a>
@@ -24,7 +24,7 @@ const HomePage = {
             chương trình đã thu hút đông đảo sinh viên Cao đẳng FPT Polytechnic tham dự. Đặc biệt các bạn sinh viên bị lôi cuốn bởi</p>
         </div>
         <div class="border border-gray-400 p-4">
-          <a href=""><img src="./img/img3.jpg" class="mx-auto" alt=""></a>
+          <a href=""><img src="../../img/img3.jpg" class="mx-auto" alt=""></a>
           <a href="">
             <h3 class="text-base text-red-600 font-bold my-2 ml-3">Sinh viên FPoly trải nghiệm thực tế tại Mường Thanh Luxury</h3>
           </a>
@@ -35,7 +35,7 @@ const HomePage = {
       <h2 class="uppercase text-lg font-bold my-4">Hoạt động sinh viên</h2>
       <div class="grid grid-cols-3 gap-8 mb-4">
         <div class="border border-gray-400 p-4 ">
-          <a href=""><img src="./img/img1.jfif" class="mx-auto" alt=""></a>
+          <a href=""><img src="../../img/img1.jfif" class="mx-auto" alt=""></a>
           <a href="">
             <h3 class="text-base text-red-600 font-bold my-2 ml-3">Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018</h3>
           </a>
@@ -43,7 +43,7 @@ const HomePage = {
             tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt</p>
         </div>
         <div class="border border-gray-400 p-4">
-          <a href=""><img src="./img/img 2.jfif" class="mx-auto " alt=""></a>
+          <a href=""><img src="../../img/img 2.jfif" class="mx-auto " alt=""></a>
           <a href="">
             <h3 class="text-base text-red-600 font-bold my-2 ml-3">Sinh viên khối ngành Kinh tế học cách “chạm” vào cảm xúc</h3>
           </a>
@@ -51,7 +51,7 @@ const HomePage = {
             chương trình đã thu hút đông đảo sinh viên Cao đẳng FPT Polytechnic tham dự. Đặc biệt các bạn sinh viên bị lôi cuốn bởi</p>
         </div>
         <div class="border border-gray-400 p-4">
-          <a href=""><img src="./img/img3.jpg" class="mx-auto" alt=""></a>
+          <a href=""><img src="../../img/img3.jpg" class="mx-auto" alt=""></a>
           <a href="">
             <h3 class="text-base text-red-600 font-bold my-2 ml-3">Sinh viên FPoly trải nghiệm thực tế tại Mường Thanh Luxury</h3>
           </a>
